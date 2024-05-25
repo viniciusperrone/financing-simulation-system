30e1d63fc455ec249678f378a88c93906d312bde
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-05-25 16:18:59 -0300

    feat: catchPropertyValue method implemented in InterfaceUser class

--- src/InterfaceUser.java
+++ src/InterfaceUser.java
@@ -1,6 +1,17 @@
-public class InterfaceUser {
-    public static void main(String [] args) {
+import java.util.Scanner;
 
+public class InterfaceUser {
+    double catchPropertyValue() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor do imovél: ");
+
+        return Double.parseDouble(scanner.nextLine());
     }
 
+    public static void main(String [] args) {
+        InterfaceUser userPrompt = new InterfaceUser();
+
+        userPrompt.catchPropertyValue();
+    }
 }
