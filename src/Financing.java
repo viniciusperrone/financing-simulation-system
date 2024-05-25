e8e26e8103ad3bf8e31153707afcd20a5c190253
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-05-25 15:26:30 -0300

    chore: constructor method added in Financig class

--- src/Financing.java
+++ src/Financing.java
@@ -3,5 +3,11 @@ public class Financing {
     int deadlineFinancing;
     double annualTaxRate;
 
+    public Financing(double propertyValue, int deadlineFinancing, double annualTaxRate) {
+        this.propertyValue = propertyValue;
+        this.deadlineFinancing = deadlineFinancing;
+        this.annualTaxRate = annualTaxRate;
+    }
+
     public static void main(String [] args) {}
 }
