29c39339ad885b3138bc22b6c4efbb19aacbf0fc
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-07-07 17:40:03 -0300

    feat: InterfaceUser improving

--- src/util/InterfaceUser.java
+++ src/util/InterfaceUser.java
@@ -107,27 +107,168 @@ public class InterfaceUser {
     }
 
     public double catchBuildingArea() {
-        return 0;
+        Scanner scanner = new Scanner(System.in);
+        boolean validEntry = false;
+        double buildingArea = 0;
+
+        while(!validEntry) {
+            System.out.print("Digite o tamanho da área construída: ");
+
+            if(scanner.hasNextDouble()) {
+                try {
+                    buildingArea = scanner.nextDouble();
+
+                    if(buildingArea <= 0) {
+                        throw new Exception("Valor deve ser positivo!");
+                    } else {
+                        validEntry = true;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+            else {
+                System.out.println("Valor inválido!");
+
+                scanner.next();
+            }
+        }
+
+        return buildingArea;
     }
 
     public double catchLandSize() {
-        return 0;
-    }
+        Scanner scanner = new Scanner(System.in);
+        boolean validEntry = false;
+        double landSize = 0;
 
-    public double catchMonthlyTax() {
-        return 0;
+        while(!validEntry) {
+            System.out.print("Digite o tamanho do terreno: ");
+
+            if(scanner.hasNextDouble()) {
+                try {
+                    landSize = scanner.nextDouble();
+
+                    if(landSize <= 0) {
+                        throw new Exception("Valor deve ser positivo!");
+                    } else {
+                        validEntry = true;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+            else {
+                System.out.println("Valor inválido!");
+
+                scanner.next();
+            }
+        }
+
+        return landSize;
     }
 
     public int catchParkingSpaces() {
-        return 0;
+        Scanner scanner = new Scanner(System.in);
+        boolean validEntry = false;
+        int parkingSpaces = 0;
+
+        while(!validEntry) {
+            System.out.print("Digite o número de vagas da garagem: ");
+
+            if(scanner.hasNextInt()) {
+                try {
+                    parkingSpaces = scanner.nextInt();
+
+                    if(parkingSpaces <= 0) {
+                        throw new Exception("Valor deve ser positivo!");
+                    } else {
+                        validEntry = true;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+            else {
+                System.out.println("Valor inválido!");
+
+                scanner.next();
+            }
+        }
+
+        return parkingSpaces;
     }
 
     public int catchFloorNumber() {
-        return 0;
+        Scanner scanner = new Scanner(System.in);
+        boolean validEntry = false;
+        int floorNumber = 0;
+
+        while(!validEntry) {
+            System.out.print("Digite o número do andar: ");
+
+            if(scanner.hasNextInt()) {
+                try {
+                    floorNumber = scanner.nextInt();
+
+                    if(floorNumber <= 0) {
+                        throw new Exception("Valor deve ser positivo!");
+                    } else {
+                        validEntry = true;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+            else {
+                System.out.println("Valor inválido!");
+
+                scanner.next();
+            }
+        }
+
+        return floorNumber;
     }
 
     public String catchKindOfZone() {
-        return new String("a");
+        Scanner scanner = new Scanner(System.in);
+        boolean validEntry = false;
+        String kindOfZone = "";
+
+        while(!validEntry) {
+            System.out.print("Digite o número do andar: ");
+
+            if(scanner.hasNext()) {
+                try {
+                    kindOfZone = scanner.next();
+
+                    if(kindOfZone.isEmpty()) {
+                        throw new Exception("Valor inválido!");
+                    } else {
+                        validEntry = true;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+            else {
+                System.out.println("Valor inválido!");
+
+                scanner.next();
+            }
+        }
+
+        return kindOfZone;
     }
 
     public static void main(String [] args) {}
